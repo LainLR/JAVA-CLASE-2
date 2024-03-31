f538c709ac59b44590912581bc3e7d640b07901b
xio <xio@ALAIN-LAP>
2024-03-31 18:56:14 -0500

    first commit

---
+++ src/c1/exe1/C1Exe1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package c1.exe1;
+
+/**
+ *
+ * @author xio
+ */
+public class C1Exe1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int miNum = 2;
+        
+        float miResultado;
+        
+        miResultado = ((2*(Math.pow(-3, miNum)))+(5*(Math.pow(2, miNum)))- (8*miNum))/(Math.sqrt(miNum))
+               
+    }
+    
+}
